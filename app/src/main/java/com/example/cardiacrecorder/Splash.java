8bc53ea7bd5f5f31c570cbb364286c20533fcf09
Abdulla Rahman <abdulla30r@gmail.com>
2023-06-27 09:13:59 +0600

    Initial commit

---
+++ app/src/main/java/com/example/cardiacrecorder/Splash.java
@@ -0,0 +1,28 @@
+package com.example.cardiacrecorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(androidx.appcompat.R.style.Theme_AppCompat_DayNight_NoActionBar);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, 1500);
+    }
+}
